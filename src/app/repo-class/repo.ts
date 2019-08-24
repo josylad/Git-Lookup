57772f297c238e634a696ffc3852c4f95db54890
Joseph Adediji <josylado@gmail.com>
2019-08-24 08:46:15 +0300

    Create a Repo class for the App

---
+++ src/app/repo-class/repo.ts
@@ -0,0 +1,11 @@
+export class Repo {
+  constructor(
+          public reponame: string,
+          public avatar : any,
+          public html_url : string,
+          public followers : string,
+          public following  : string,
+          public repos : any,
+
+        ){}
+      }
