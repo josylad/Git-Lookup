ebb137e5c1a20db95d54666ca526d2dad1336c39
Joseph Adediji <josylado@gmail.com>
2019-08-24 08:49:14 +0300

    Update App module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitNavbarComponent } from './git-navbar/git-navbar.component';
+import { GithomeComponent } from './githome/githome.component';
+import { GitFormComponent } from './git-form/git-form.component';
+import { HighlightDirective } from './highlight.directive';
+// import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitNavbarComponent,
+    GithomeComponent,
+    GitFormComponent,
+    HighlightDirective,
+    // GithubComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
