01f247e73815fda7687dccb45334ae645a78d6e6
Joseph Adediji <josylado@gmail.com>
2019-08-24 09:21:40 +0300

    Update the user.ts file and fix errors

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -3,7 +3,7 @@ export class User {
           public username:string,
           public avatar: string,
           public html_url: string,
-          public name: string'
+          public name: string,
           public followers:number,
           public following: number,
           public url: string,
