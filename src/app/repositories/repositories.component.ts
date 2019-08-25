384c45a22276bca4e43fb651ffe5a8f919c34860
Joseph Adediji <josylado@gmail.com>
2019-08-25 07:08:29 +0300

    Create a Repositories Component for the repository search page

---
+++ src/app/repositories/repositories.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {GitserviceService} from '../services/gitservice.service'
+import {Github} from '../github';
+import { Gitrepo } from '../gitrepo';
+
+@Component({
+  selector: 'app-repositories',
+  templateUrl: './repositories.component.html',
+  styleUrls: ['./repositories.component.css']
+})
+export class RepositoriesComponent implements OnInit {
+  gitrepos:Gitrepo[];
+
+    constructor(public Gitservice: GitserviceService) {
+     }
+
+
+    ngOnInit() {
+      this.searchRepo("josylad");
+    }
+
+    searchRepo(searchTerm){
+      this.Gitservice.searchRepo(searchTerm).then(
+        ()=>{
+          this.gitrepos=this.Gitservice.gitrepos;
+          // console.log(this.gitrepos);
+
+        },
+        (error)=>{
+          console.log(error)
+        }
+      )
+      // console.log(searchTerm)
+    }
+  }
