db8512ef87747ff19fa7697115805bd62d182f29
Joseph Adediji <josylado@gmail.com>
2019-08-25 07:03:53 +0300

    Update Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GitserviceService} from './services/gitservice.service';
+import {Github} from './github';
+import {RepositoriesComponent} from './repositories/repositories.component';
+import {GithomeComponent} from './githome/githome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profiles', component: GithomeComponent},
+  { path: 'repos', component: RepositoriesComponent},
+  { path: '', redirectTo:"/profiles", pathMatch:"full"},
+  { path:'**', component:GithomeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
